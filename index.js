b80eb66036be4d1cc6ca2e6239c95b7f7005efd0
sneoo <epicshakil@gmail.com>
2022-11-08 01:47:37 +0600

    added services and limited services

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+
+//middle wares
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wdkgpff.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client
+      .db("speedy-service")
+      .collection("services");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/services-limited", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const limitedService = await cursor.skip(0).limit(3).toArray();
+      res.send(limitedService);
+    });
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, (req, res) => {
+  console.log(`server is running on ${port}`);
+});
