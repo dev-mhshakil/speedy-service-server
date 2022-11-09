862834dd14051c188e6c2ee8d4990733a1ee0681
sneoo <epicshakil@gmail.com>
2022-11-10 00:58:00 +0600

    updated token expire time

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function run() {
     app.post("/jwt", (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
+        expiresIn: "30d",
       });
       res.send({ token });
     });
